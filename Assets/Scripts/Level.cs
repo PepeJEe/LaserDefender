ef31a498f409d696539f2927f7f49dec9b74ff66
PepeJEe <perttu.vierimaa@gmail.com>
2025-01-07 01:46:17 +0200

    Initial commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] float delay = 1f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        GameSession gameSession = FindObjectOfType<GameSession>();
+        if (gameSession)
+        {
+            gameSession.ResetGame();
+        }
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Game Over");
+    }
+
+}
